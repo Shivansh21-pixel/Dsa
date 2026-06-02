62511753c0b8105cc61c900c68b74314720c20b9
Shivansh pandey <shivanshpandey210501@gmail.com>
2026-06-02 15:29:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (30.07%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>pascal(numRows);
+        for(int i=0;i<numRows;i++){
+            pascal[i]=vector<int>(i+1,1);
+            for(int j=1;j<i;j++){
+                pascal[i][j]=pascal[i-1][j]+pascal[i-1][j-1];
+            }
+        }
+    return pascal;
+    }
+};
